17781713a286eb256d3ebe196ab6f95c72dad1e1
kyumoto <kyumoto.xxx.yyy.zzz@docomo.ne.jp>
2016-06-29 21:45:16 +0900

    Add any project.
    -Service
    -Interface Adapter
    -Test.Domain

---
+++ CleanArchitecture/WpfInterfaceAdapters/Exchanges/PresetationItemExchanger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YCP.Exchanges
+{
+    /// <summary>
+    /// Domain-LayerのオブジェクトをPresentation-Layerのオブジェクトへ変換する
+    /// </summary>
+    public static class PresetationItemExchanger
+    {
+    }
+}
