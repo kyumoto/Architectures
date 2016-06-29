5204c191bf71bd5e564712db564750438687d36a
kyumoto <kyumoto.xxx.yyy.zzz@docomo.ne.jp>
2016-06-29 21:37:42 +0900

    Add UnitTest Project.

---
+++ CleanArchitecture/Test.Domains/DomainTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Domains
+{
+    [TestClass]
+    public class DomainTest
+    {
+        [TestMethod]
+        public void Check()
+        {
+        }
+    }
+}
